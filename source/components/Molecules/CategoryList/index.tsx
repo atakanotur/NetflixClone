ff30930f39326597f447a8e8f14b4545d289c525
Atakan <42267756+atakanotur@users.noreply.github.com>
2024-08-01 16:45:48 +0300

    Update index.tsx

--- source/components/Molecules/CategoryList/index.tsx
+++ source/components/Molecules/CategoryList/index.tsx
@@ -1,19 +1,30 @@
 import { ComponentType, ReactElement } from "react";
 import { FlashList, ListRenderItem } from "@shopify/flash-list";
+import { NativeScrollEvent, NativeSyntheticEvent } from "react-native";
 
 type CategoryListProps = {
     data: Category[];
     renderItem: ListRenderItem<Category>
     ListHeaderComponent?: ComponentType<any> | ReactElement<any, string | React.JSXElementConstructor<any>>
+    onScroll?: ((event: NativeSyntheticEvent<NativeScrollEvent>) => void) | undefined
+    onScrollAnimationEnd?: (() => void) | undefined;
+    onScrollBeginDrag?: ((event: NativeSyntheticEvent<NativeScrollEvent>) => void) | undefined
+    onScrollEndDrag?: ((event: NativeSyntheticEvent<NativeScrollEvent>) => void) | undefined
+    onScrollToTop?: ((event: NativeSyntheticEvent<NativeScrollEvent>) => void) | undefined
 }
 
-const CategoryList = ({ data, renderItem, ListHeaderComponent }: CategoryListProps) => {
+const CategoryList = ({ data, renderItem, ListHeaderComponent, onScroll, onScrollAnimationEnd, onScrollBeginDrag, onScrollEndDrag, onScrollToTop }: CategoryListProps) => {
     return (
         <FlashList
             data={data}
             renderItem={renderItem}
             ListHeaderComponent={ListHeaderComponent}
             estimatedItemSize={300}
+            onScroll={onScroll}
+            onScrollAnimationEnd={onScrollAnimationEnd}
+            onScrollBeginDrag={onScrollBeginDrag}
+            onScrollEndDrag={onScrollEndDrag}
+            onScrollToTop={onScrollToTop}
         />
     )
 }
