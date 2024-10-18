8b22ea33208f46a8c315a76beeb8f2d1e4fdafa8
Atakan <42267756+atakanotur@users.noreply.github.com>
2024-10-18 15:32:14 +0300

    expand animation v1

--- source/components/Molecules/ContentListRenderItem/detailMode.tsx
+++ source/components/Molecules/ContentListRenderItem/detailMode.tsx
@@ -13,13 +13,13 @@ import Constant from 'expo-constants';
 import Animated, { useAnimatedStyle, withTiming, useSharedValue } from 'react-native-reanimated';
 import categoryStore from '@/source/store/categoryStore';
 import { BlurView } from 'expo-blur';
-import { z } from 'zod';
 
 type DetailModeProps = {
     content: Series | Movie;
     onClose: () => void;
     collapsibleWidth: number;
     collapsibleHeight: number;
+    animationDuration: number;
 }
 
 export type DetailModeRef = {
@@ -30,7 +30,7 @@ export type DetailModeRef = {
 const { width, height } = Dimensions.get("window");
 const { statusBarHeight } = Constant;
 
-const DetailMode = forwardRef(({ content, onClose, collapsibleWidth, collapsibleHeight }: DetailModeProps, ref) => {
+const DetailMode = forwardRef(({ content, onClose, collapsibleWidth, collapsibleHeight, animationDuration }: DetailModeProps, ref) => {
     useImperativeHandle(ref, () => ({
         animateOpen,
         animateClose
@@ -38,7 +38,7 @@ const DetailMode = forwardRef(({ content, onClose, collapsibleWidth, collapsible
 
     const categories = categoryStore(state => state.categories);
 
-    const [visible, setVisible] = useState<boolean>(false);
+    const [expanded, setExpanded] = useState<boolean>(false);
     const [similarOrEpisodesSelected, setSimilarOrEpisodesSelected] = useState<"similar" | "episodes">(content.type === "series" ? "episodes" : "similar");
     const [fullscreen, setFullscreen] = useState<boolean>(false);
 
@@ -48,18 +48,19 @@ const DetailMode = forwardRef(({ content, onClose, collapsibleWidth, collapsible
 
     const AnimatedBlurView = Animated.createAnimatedComponent(BlurView);
 
-    const baseAnimationDuration: number = 1000;
-
     // Container Animation
+    const detailPaddingTop = useSharedValue(statusBarHeight);
     const detailHeight = useSharedValue(0);
     const detailWidth = useSharedValue(0);
     const detailZIndex = useSharedValue(0);
     const detailStyle = useAnimatedStyle(() => {
         return {
+            paddingTop: detailPaddingTop.value,
+            borderTopRightRadius: 15,
+            borderTopLeftRadius: 15,
             height: detailHeight.value,
             width: detailWidth.value + 1,
-            zIndex: detailZIndex.value
-
+            zIndex: detailZIndex.value,
         }
     });
 
@@ -71,8 +72,6 @@ const DetailMode = forwardRef(({ content, onClose, collapsibleWidth, collapsible
     const detailBlurViewStyle = useAnimatedStyle(() => {
         return {
             position: 'absolute',
-            top: 0,
-            left: 0,
             height: detailBlurViewHeight.value,
             width: detailBlurViewWidth.value,
             zIndex: detailBlurViewZIndex.value,
@@ -86,11 +85,9 @@ const DetailMode = forwardRef(({ content, onClose, collapsibleWidth, collapsible
     const posterStyle = useAnimatedStyle(() => {
         return {
             position: "absolute",
-            top: 0,
-            left: 0,
             height: posterHeight.value,
             width: posterWidth.value,
-            zIndex: posterZIndex.value
+            zIndex: posterZIndex.value,
         }
     });
 
@@ -102,8 +99,6 @@ const DetailMode = forwardRef(({ content, onClose, collapsibleWidth, collapsible
     const posterBlurViewStyle = useAnimatedStyle(() => {
         return {
             position: 'absolute',
-            top: 0,
-            left: 0,
             height: posterBlurViewHeight.value,
             width: posterBlurViewWidth.value,
             zIndex: posterBlurViewZIndex.value,
@@ -112,49 +107,52 @@ const DetailMode = forwardRef(({ content, onClose, collapsibleWidth, collapsible
 
 
     const animateOpen = () => {
-        setVisible(true);
-        detailHeight.value = withTiming(height, { duration: baseAnimationDuration });
-        detailWidth.value = withTiming(width, { duration: baseAnimationDuration });
-        detailZIndex.value = withTiming(3, { duration: baseAnimationDuration });
+        setExpanded(true);
 
-        detailBlurViewHeight.value = withTiming(height, { duration: baseAnimationDuration });
-        detailBlurViewWidth.value = withTiming(width, { duration: baseAnimationDuration });
-        detailBlurViewZIndex.value = withTiming(2, { duration: baseAnimationDuration / 2 });
-        detailBlurIntensity.value = withTiming(0, { duration: baseAnimationDuration });
+        detailPaddingTop.value = withTiming(0, { duration: animationDuration });
+        detailHeight.value = withTiming(height, { duration: animationDuration });
+        detailWidth.value = withTiming(width, { duration: animationDuration });
+        detailZIndex.value = withTiming(3, { duration: animationDuration });
 
-        posterHeight.value = withTiming(height, { duration: baseAnimationDuration });
-        posterWidth.value = withTiming(width, { duration: baseAnimationDuration });
-        posterZIndex.value = withTiming(0, { duration: baseAnimationDuration });
+        detailBlurViewHeight.value = withTiming(height, { duration: animationDuration });
+        detailBlurViewWidth.value = withTiming(width, { duration: animationDuration });
+        detailBlurViewZIndex.value = withTiming(2, { duration: animationDuration / 2 });
+        detailBlurIntensity.value = withTiming(0, { duration: animationDuration });
 
-        posterBlurViewHeight.value = withTiming(height, { duration: baseAnimationDuration });
-        posterBlurViewWidth.value = withTiming(width, { duration: baseAnimationDuration });
-        posterBlurViewZIndex.value = withTiming(0, { duration: baseAnimationDuration });
-        posterBlurIntensity.value = withTiming(100, { duration: baseAnimationDuration + 10 });
+        posterHeight.value = withTiming(height, { duration: animationDuration });
+        posterWidth.value = withTiming(width, { duration: animationDuration });
+        posterZIndex.value = withTiming(0, { duration: animationDuration });
+
+        posterBlurViewHeight.value = withTiming(height, { duration: animationDuration });
+        posterBlurViewWidth.value = withTiming(width, { duration: animationDuration });
+        posterBlurViewZIndex.value = withTiming(0, { duration: animationDuration });
+        posterBlurIntensity.value = withTiming(100, { duration: animationDuration });
     }
 
     const animateClose = () => {
         onClose();
         setTimeout(() => {
-            setVisible(false);
-        }, baseAnimationDuration - 10);
+            setExpanded(false);
+        }, animationDuration / 2);
 
-        detailHeight.value = withTiming(collapsibleHeight, { duration: baseAnimationDuration });
-        detailWidth.value = withTiming(collapsibleWidth, { duration: baseAnimationDuration });
-        detailZIndex.value = withTiming(0, { duration: baseAnimationDuration });
+        detailPaddingTop.value = withTiming(0, { duration: animationDuration });
+        detailHeight.value = withTiming(collapsibleHeight, { duration: animationDuration });
+        detailWidth.value = withTiming(collapsibleWidth, { duration: animationDuration });
+        detailZIndex.value = withTiming(0, { duration: animationDuration });
 
-        detailBlurViewHeight.value = withTiming(collapsibleHeight, { duration: baseAnimationDuration });
-        detailBlurViewWidth.value = withTiming(collapsibleWidth, { duration: baseAnimationDuration });
-        detailBlurViewZIndex.value = withTiming(0, { duration: baseAnimationDuration });
-        detailBlurIntensity.value = withTiming(100, { duration: baseAnimationDuration + 10 });
+        detailBlurViewHeight.value = withTiming(0, { duration: animationDuration });
+        detailBlurViewWidth.value = withTiming(collapsibleHeight, { duration: animationDuration });
+        detailBlurViewZIndex.value = withTiming(collapsibleWidth, { duration: animationDuration });
+        detailBlurIntensity.value = withTiming(100, { duration: animationDuration });
 
-        posterHeight.value = withTiming(collapsibleHeight, { duration: baseAnimationDuration });
-        posterWidth.value = withTiming(collapsibleWidth, { duration: baseAnimationDuration });
-        posterZIndex.value = withTiming(2, { duration: baseAnimationDuration });
+        posterHeight.value = withTiming(collapsibleHeight, { duration: animationDuration });
+        posterWidth.value = withTiming(collapsibleWidth, { duration: animationDuration });
+        posterZIndex.value = withTiming(3, { duration: animationDuration });
 
-        posterBlurViewHeight.value = withTiming(collapsibleHeight, { duration: baseAnimationDuration });
-        posterBlurViewWidth.value = withTiming(collapsibleWidth, { duration: baseAnimationDuration });
-        posterBlurViewZIndex.value = withTiming(3, { duration: baseAnimationDuration / 2 });
-        posterBlurIntensity.value = withTiming(0, { duration: baseAnimationDuration });
+        posterBlurViewHeight.value = withTiming(collapsibleHeight, { duration: animationDuration });
+        posterBlurViewWidth.value = withTiming(collapsibleWidth, { duration: animationDuration });
+        posterBlurViewZIndex.value = withTiming(4, { duration: animationDuration / 2 });
+        posterBlurIntensity.value = withTiming(0, { duration: animationDuration });
     }
 
     const getCategoryByContentId = (contentId: string) => {
@@ -172,81 +170,72 @@ const DetailMode = forwardRef(({ content, onClose, collapsibleWidth, collapsible
         setEpisodesButtonWidth(width);
     }
 
-    if (!visible) {
-        return;
-    }
-
     return (
-        <Animated.View style={[styles.detailModeContainer, detailStyle]}>
+        <Animated.View style={detailStyle}>
             <AnimatedBlurView style={[posterBlurViewStyle]} intensity={posterBlurIntensity} />
             <Animated.Image source={{ uri: content.poster }} style={posterStyle} />
             <AnimatedBlurView style={[detailBlurViewStyle]} intensity={detailBlurIntensity} />
-            <Ionicons name="close" size={responsiveFontSize(24)} color={colors.white} onPress={animateClose} style={styles.closeIcon} />
-            <VideoPlayer
-                fullscreen={fullscreen}
-                videoSource={content.trailer}
-                visible={true}
-                style={styles.video}
-            />
-            <ScrollView style={styles.detailModeInformationContainer} contentContainerStyle={styles.detailModeInformationContainerContent} showsVerticalScrollIndicator={false}>
-                <Text text={content.title} style={styles.contentTitle} />
-                <View style={styles.yearAndSeasonNumberOrMovieDuration}>
-                    <Text text={content.year.toString()} style={styles.year} />
-                    <MaterialIcons name='18-up-rating' size={responsiveFontSize(24)} style={styles.yearAndSeasonNumberOrMovieDurationIcon} />
-                    <Text text={content.type === "movie" ? "2 h. 11 min." : content.numberOfSeasons + " Season"} style={styles.seasonNumberOrMovieDuration} />
-                    <MaterialIcons name='hd' size={responsiveFontSize(24)} style={styles.yearAndSeasonNumberOrMovieDurationIcon} />
-                </View>
-                <View style={styles.playAndDownloadContainer}>
-                    <Pressable style={[styles.playAndDownloadButton, { backgroundColor: colors.white }]}>
-                        <Ionicons name="play-sharp" size={responsiveFontSize(25)} color={colors.black} />
-                        <Text text={localization.t("play")} style={styles.play} />
-                    </Pressable>
-                    <Pressable style={[styles.playAndDownloadButton, { backgroundColor: colors.grey }]}>
-                        <Octicons name="download" size={responsiveFontSize(20)} color={colors.white} />
-                        <Text text={localization.t("download")} style={styles.download} />
-                    </Pressable>
-                </View>
-                <Text text={content.plot} style={styles.plot} />
-                <Text text={`Cast : ${content.cast}`} style={styles.cast} />
-                <Text text={`Creator : ${content.creator}`} style={styles.creator} />
-                <View style={styles.actionButtonContainer}>
-                    <Pressable style={styles.actionButton}>
-                        <Ionicons name={listAdded ? "checkmark" : "add"} size={responsiveFontSize(25)} color={colors.whiteGrey} />
-                        <Text text={localization.t("myList")} style={styles.actionButtonText} />
-                    </Pressable>
-                    <Pressable style={styles.actionButton}>
-                        <Feather name="thumbs-up" size={responsiveFontSize(25)} color={colors.whiteGrey} />
-                        <Text text={localization.t("giveScore")} style={styles.actionButtonText} />
-                    </Pressable>
-                    <Pressable style={styles.actionButton}>
-                        <Ionicons name="paper-plane-outline" size={responsiveFontSize(25)} color={colors.whiteGrey} />
-                        <Text text={localization.t("recommend")} style={styles.actionButtonText} />
-                    </Pressable>
-                </View>
-                <View style={styles.similarAndEpisodesContainer}>
-                    {content.type === "series" &&
-                        <Pressable style={[styles.similarAndEpisodesButton, styles.episodesButton]} onLayout={onEpisodesButtonLayout} onPress={() => setSimilarOrEpisodesSelected("episodes")}>
-                            <RedBarAnimation isSelected={similarOrEpisodesSelected === "episodes"} width={episodesButtonWidth} />
-                            <Text text={localization.t("episodes")} />
-                        </Pressable>
-                    }
-                    <Pressable style={[styles.similarAndEpisodesButton, styles.similarButton]} onLayout={onSimilarButtonLayout} onPress={() => setSimilarOrEpisodesSelected("similar")}>
-                        <RedBarAnimation isSelected={similarOrEpisodesSelected === "similar"} width={similarButtonWidth} />
-                        <Text text={localization.t("similar")} />
-                    </Pressable>
-                </View>
-                {content.type === "series" && similarOrEpisodesSelected === "episodes" ? <EpisodeList seasons={content.seasons} /> : <SimilarContentList similarContent={similarContent} />}
-            </ScrollView>
+            {expanded &&
+                <>
+                    <Ionicons name="close" size={responsiveFontSize(24)} color={colors.white} onPress={animateClose} style={styles.closeIcon} /><VideoPlayer
+                        fullscreen={fullscreen}
+                        videoSource={content.trailer}
+                        visible={true}
+                        style={styles.video} /><ScrollView style={styles.detailModeInformationContainer} contentContainerStyle={styles.detailModeInformationContainerContent} showsVerticalScrollIndicator={false}>
+                        <Text text={content.title} style={styles.contentTitle} />
+                        <View style={styles.yearAndSeasonNumberOrMovieDuration}>
+                            <Text text={content.year.toString()} style={styles.year} />
+                            <MaterialIcons name='18-up-rating' size={responsiveFontSize(24)} style={styles.yearAndSeasonNumberOrMovieDurationIcon} />
+                            <Text text={content.type === "movie" ? "2 h. 11 min." : content.numberOfSeasons + " Season"} style={styles.seasonNumberOrMovieDuration} />
+                            <MaterialIcons name='hd' size={responsiveFontSize(24)} style={styles.yearAndSeasonNumberOrMovieDurationIcon} />
+                        </View>
+                        <View style={styles.playAndDownloadContainer}>
+                            <Pressable style={[styles.playAndDownloadButton, { backgroundColor: colors.white }]}>
+                                <Ionicons name="play-sharp" size={responsiveFontSize(25)} color={colors.black} />
+                                <Text text={localization.t("play")} style={styles.play} />
+                            </Pressable>
+                            <Pressable style={[styles.playAndDownloadButton, { backgroundColor: colors.grey }]}>
+                                <Octicons name="download" size={responsiveFontSize(20)} color={colors.white} />
+                                <Text text={localization.t("download")} style={styles.download} />
+                            </Pressable>
+                        </View>
+                        <Text text={content.plot} style={styles.plot} />
+                        <Text text={`Cast : ${content.cast}`} style={styles.cast} />
+                        <Text text={`Creator : ${content.creator}`} style={styles.creator} />
+                        <View style={styles.actionButtonContainer}>
+                            <Pressable style={styles.actionButton}>
+                                <Ionicons name={listAdded ? "checkmark" : "add"} size={responsiveFontSize(25)} color={colors.whiteGrey} />
+                                <Text text={localization.t("myList")} style={styles.actionButtonText} />
+                            </Pressable>
+                            <Pressable style={styles.actionButton}>
+                                <Feather name="thumbs-up" size={responsiveFontSize(25)} color={colors.whiteGrey} />
+                                <Text text={localization.t("giveScore")} style={styles.actionButtonText} />
+                            </Pressable>
+                            <Pressable style={styles.actionButton}>
+                                <Ionicons name="paper-plane-outline" size={responsiveFontSize(25)} color={colors.whiteGrey} />
+                                <Text text={localization.t("recommend")} style={styles.actionButtonText} />
+                            </Pressable>
+                        </View>
+                        <View style={styles.similarAndEpisodesContainer}>
+                            {content.type === "series" &&
+                                <Pressable style={[styles.similarAndEpisodesButton, styles.episodesButton]} onLayout={onEpisodesButtonLayout} onPress={() => setSimilarOrEpisodesSelected("episodes")}>
+                                    <RedBarAnimation isSelected={similarOrEpisodesSelected === "episodes"} width={episodesButtonWidth} />
+                                    <Text text={localization.t("episodes")} />
+                                </Pressable>}
+                            <Pressable style={[styles.similarAndEpisodesButton, styles.similarButton]} onLayout={onSimilarButtonLayout} onPress={() => setSimilarOrEpisodesSelected("similar")}>
+                                <RedBarAnimation isSelected={similarOrEpisodesSelected === "similar"} width={similarButtonWidth} />
+                                <Text text={localization.t("similar")} />
+                            </Pressable>
+                        </View>
+                        {content.type === "series" && similarOrEpisodesSelected === "episodes" ? <EpisodeList seasons={content.seasons} /> : <SimilarContentList similarContent={similarContent} />}
+                    </ScrollView>
+                </>
+            }
         </Animated.View>
     )
 })
 
 const styles = StyleSheet.create({
-    detailModeContainer: {
-        paddingTop: statusBarHeight,
-        borderTopRightRadius: 15,
-        borderTopLeftRadius: 15,
-    },
     video: {
         borderTopRightRadius: 15,
         borderTopLeftRadius: 15,
@@ -341,7 +330,7 @@ const styles = StyleSheet.create({
 
     },
     similarButton: {
-        
+
     },
     episodesButton: {
         marginRight: 20
--- source/components/Molecules/ContentListRenderItem/posterMode.tsx
+++ source/components/Molecules/ContentListRenderItem/posterMode.tsx
@@ -28,8 +28,9 @@ const PosterMode = forwardRef(({ content }: PosterModeProps, ref) => {
     const AnimatedModal = Animated.createAnimatedComponent(Modal);
 
     const [isDetailMode, setIsDetailMode] = useState<boolean>(false);
+    const [expanded, setExpanded] = useState<boolean>(false);
 
-    const baseAnimationDuration: number = 1000;
+    const animationDuration: number = 400;
 
     //Modal Animation Styles
     const modalAnimationHeight = useSharedValue(posterHeight);
@@ -43,7 +44,7 @@ const PosterMode = forwardRef(({ content }: PosterModeProps, ref) => {
             width: modalAnimationWidth.value,
             alignItems: 'center',
             justifyContent: 'center',
-            position: modalAnimationPosition.value,
+            borderRadius: 5
         }
     });
 
@@ -53,6 +54,7 @@ const PosterMode = forwardRef(({ content }: PosterModeProps, ref) => {
 
     const modalContainerAnimationStyle = useAnimatedStyle(() => {
         return {
+            borderRadius: 15,
             height: modalAnimationHeight.value,
             width: modalAnimationWidth.value,
             alignItems: 'center',
@@ -97,42 +99,48 @@ const PosterMode = forwardRef(({ content }: PosterModeProps, ref) => {
 
     const animateDetailModeOpen = () => {
         setIsDetailMode(true);
+        setExpanded(true);
         modalAnimationPosition.value = "absolute";
 
-        modalAnimationHeight.value = withTiming(height, { duration: baseAnimationDuration });
-        modalAnimationWidth.value = withTiming(width, { duration: baseAnimationDuration });
+        modalAnimationHeight.value = withTiming(height, { duration: animationDuration });
+        modalAnimationWidth.value = withTiming(width, { duration: animationDuration });
 
-        modalContainerAnimationTop.value = withTiming(0, { duration: baseAnimationDuration });
-        modalContainerAnimationLeft.value = withTiming(0, { duration: baseAnimationDuration });
+        modalContainerAnimationTop.value = withTiming(0, { duration: animationDuration });
+        modalContainerAnimationLeft.value = withTiming(0, { duration: animationDuration });
 
-        posterAnimationHeight.value = withTiming(height, { duration: baseAnimationDuration });
-        posterAnimationWidth.value = withTiming(width, { duration: baseAnimationDuration });
+        posterAnimationHeight.value = withTiming(height, { duration: animationDuration });
+        posterAnimationWidth.value = withTiming(width, { duration: animationDuration });
     }
 
     const animateDetailModeClose = () => {
         setTimeout(() => {
             setIsDetailMode(false)
             modalAnimationPosition.value = "relative";
-        }, baseAnimationDuration);
+        }, animationDuration - 70);
 
-        modalAnimationHeight.value = withTiming(posterHeight, { duration: baseAnimationDuration });
-        modalAnimationWidth.value = withTiming(posterWidth, { duration: baseAnimationDuration });
+        modalAnimationHeight.value = withTiming(posterHeight, { duration: animationDuration });
+        modalAnimationWidth.value = withTiming(posterWidth, { duration: animationDuration });
 
-        modalContainerAnimationTop.value = withTiming(modalAnimationTempTop, { duration: baseAnimationDuration });
-        modalContainerAnimationLeft.value = withTiming(modalAnimationTempLeft + posterWidth + 15, { duration: baseAnimationDuration });
+        modalContainerAnimationTop.value = withTiming(modalAnimationTempTop, { duration: animationDuration });
+        modalContainerAnimationLeft.value = withTiming(modalAnimationTempLeft + posterWidth + 15, { duration: animationDuration });
 
-        posterAnimationHeight.value = withTiming(posterHeight, { duration: baseAnimationDuration });
-        posterAnimationWidth.value = withTiming(posterWidth, { duration: baseAnimationDuration });
+        posterAnimationHeight.value = withTiming(posterHeight, { duration: animationDuration });
+        posterAnimationWidth.value = withTiming(posterWidth, { duration: animationDuration });
     }
 
     if (isDetailMode) {
         return (
             <>
                 <AnimatedModal visible={isDetailMode} style={modalAnimationStyle} transparent animationType="none">
-                    <Animated.View style={modalContainerAnimationStyle} ref={containerRef}>
-                        <Animated.View style={styles.content}>
-                            <DetailMode content={content} onClose={animateDetailModeClose} collapsibleWidth={posterWidth} collapsibleHeight={posterHeight} ref={detailModeRef} />
-                        </Animated.View>
+                    <Animated.View style={modalContainerAnimationStyle}>
+                        {expanded && <DetailMode
+                            content={content}
+                            onClose={animateDetailModeClose}
+                            collapsibleWidth={posterWidth}
+                            collapsibleHeight={posterHeight}
+                            animationDuration={animationDuration - 100}
+                            ref={detailModeRef}
+                        />}
                     </Animated.View>
                 </AnimatedModal>
                 <View style={styles.container}>
@@ -144,7 +152,7 @@ const PosterMode = forwardRef(({ content }: PosterModeProps, ref) => {
 
     return (
         <View style={styles.container} ref={containerRef}>
-            <Pressable onPress={posterOnPress} style={styles.content}>
+            <Pressable onPress={posterOnPress}>
                 <Animated.Image source={{ uri: content.poster }} style={posterAnimationStyle} resizeMode="stretch" />
             </Pressable>
         </View>
@@ -156,15 +164,6 @@ const styles = StyleSheet.create({
         height: posterHeight,
         width: posterWidth,
         margin: 5,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    content: {
-        height: posterHeight,
-        width: posterWidth,
-        margin: 5,
-        alignItems: 'center',
-        justifyContent: 'center'
     },
     poster: {
         height: posterHeight,
